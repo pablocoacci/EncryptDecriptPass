46be73afadb316e900c858900bed16fe05cfae0d
pcoacci <pcoacci@originsoftware.com.ar>
2019-07-04 17:55:02 -0300

    agregue un nuevo controlador para gestionar el login del usuario

---
+++ EncryptDecriptPassWebAngular/Controllers/DecryptEncryptController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EncryptDescript.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptDecriptPassWebAngular.Controllers
+{
+    [Route("api/[controller]")]
+    public class DecryptEncryptController : Controller
+    {
+        private readonly string _passEntitiesFilePath = @"C:\\TestProjects\\EncryptDecript\\PassUserPasswords\\";
+
+        [HttpGet("[action]")]
+        public JsonResult LoginUser(string userName, string password)
+        {
+            EncryptDecrypLib.IEncrypterDecrypter ed = new EncryptDecrypLib.Encrypters.EncrypterVigenere();
+            FileManager fileManager = new FileManager(ed);
+            var errorDescLoad = fileManager.LoadJsonPassEntitiesFileAsync(_passEntitiesFilePath, userName, password, true).Result;
+
+            return Json(errorDescLoad);
+        }
+    }
+}
