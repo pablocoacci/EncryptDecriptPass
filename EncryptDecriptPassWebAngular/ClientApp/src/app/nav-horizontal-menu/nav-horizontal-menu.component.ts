01181b6ea118002eb818af0bcc6a1fd7f74219d1
pcoacci <pcoacci@originsoftware.com.ar>
2019-07-10 15:04:05 -0300

    Agregue un post por typesript

--- EncryptDecriptPassWebAngular/ClientApp/src/app/nav-horizontal-menu/nav-horizontal-menu.component.ts
+++ EncryptDecriptPassWebAngular/ClientApp/src/app/nav-horizontal-menu/nav-horizontal-menu.component.ts
@@ -1,5 +1,6 @@
 
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-nav-horizontal-menu',
@@ -10,10 +11,24 @@ export class NavHorizontalMenu {
   //usuarioNombre: string;
   usuarioNombre = '';
   password = '';
+  httpClient: HttpClient;
+  urlService: string;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.httpClient = http;
+    this.urlService = baseUrl;
+  }
 
   public LoginUser() {
-    alert('hola que tal');
+    //alert('hola que tal');
 
-    alert(this.usuarioNombre + ' ' + this.password);
+    //alert(this.usuarioNombre + ' ' + this.password);
+
+    var urlTest = this.urlService +'api/DecryptEncrypt/LoginUser'
+
+    this.httpClient.post(urlTest, { UserName: this.usuarioNombre, Password: this.password }).subscribe(result => {
+      alert(result);
+    }, error => console.error(error));
+    
   }
 }
--- EncryptDecriptPassWebAngular/Controllers/DecryptEncryptController.cs
+++ EncryptDecriptPassWebAngular/Controllers/DecryptEncryptController.cs
@@ -7,19 +7,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EncryptDecriptPassWebAngular.Controllers
 {
+    public class RquestTest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
     [Route("api/[controller]")]
     public class DecryptEncryptController : Controller
     {
         private readonly string _passEntitiesFilePath = @"C:\\TestProjects\\EncryptDecript\\PassUserPasswords\\";
 
-        [HttpGet("[action]")]
-        public JsonResult LoginUser(string userName, string password)
+        [HttpPost("[action]")]
+        public JsonResult LoginUser([FromBody] RquestTest rq)
         {
             EncryptDecrypLib.IEncrypterDecrypter ed = new EncryptDecrypLib.Encrypters.EncrypterVigenere();
             FileManager fileManager = new FileManager(ed);
-            var errorDescLoad = fileManager.LoadJsonPassEntitiesFileAsync(_passEntitiesFilePath, userName, password, true).Result;
+            var errorDescLoad = fileManager.LoadJsonPassEntitiesFileAsync(_passEntitiesFilePath, rq.UserName, rq.Password, true).Result;
 
             return Json(errorDescLoad);
         }
+        
     }
 }
