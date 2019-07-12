cb25f7786fd5943787f159532291525e4da43521
pcoacci <pcoacci@originsoftware.com.ar>
2019-07-12 12:51:09 -0300

    Agregue el nuevo componente de angular para listar las passentities. Por ahora el componente esta vacio

---
+++ EncryptDecriptPassWebAngular/ClientApp/src/app/common-scripts/PassEntityInterface.ts
@@ -0,0 +1,13 @@
+export interface PassEntity {
+  Id: number;
+  EncryptDecryptPass: string;
+  Usuario: string;
+  Descripcion: string;
+  Sitio: string;
+  Cuenta: string;
+  PassWord: string
+  PreguntaSecreta: string;
+  RespuestaSecreta: string;
+  MailContacto: string;
+  IsEncrypter: boolean;
+}
